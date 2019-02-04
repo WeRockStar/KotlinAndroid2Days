f17b5007dd1fb4561654d84dc67c5a55d42936d7
WeRockStar <kotchaphan.m@gmail.com>
2019-02-04 13:23:27 +0700

    Initial commit

---
+++ app/src/main/java/com/werockstar/kotlin2days/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.werockstar.kotlin2days
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        /**
+         *  Retrofit
+         *  OKHttp
+         *  RxJava
+         *  Mockito kotlin
+         *  Gson
+         */
+
+    }
+}
