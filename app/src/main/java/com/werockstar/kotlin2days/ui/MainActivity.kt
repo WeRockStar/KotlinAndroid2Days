fee5d70201e66b5f7d5c76e392d5ebca7dd66992
WeRockStar <kotchaphan.m@gmail.com>
2019-02-05 13:53:44 +0700

    Remove unused

--- app/src/main/java/com/werockstar/kotlin2days/ui/MainActivity.kt
+++ app/src/main/java/com/werockstar/kotlin2days/ui/MainActivity.kt
@@ -46,22 +46,8 @@ class MainActivity : AppCompatActivity(), GithubView {
 
     }
 
-    data class Person(val name: String, val age: Int)
 
     override fun showLoading() {
-        val users = listOf(
-            Person("WeRockStar", 25),
-            Person("Aou", 20),
-            Person("Title", 22)
-        )
-
-        val list = users.filter { it.age >= 20 }
-            .filter { it.name.length > 3 }
-            .map { Person("Mr.${it.name}", it.age) }
-            .map { it.name }
-
-
-        println(list.toString())
     }
 
     override fun onUserError(message: String?) {
@@ -76,11 +62,5 @@ class MainActivity : AppCompatActivity(), GithubView {
         super.onDestroy()
 
         presenter.onDestroy()
-
-        val sum = 1 บวก 2
-    }
-
-    infix fun Int.บวก(b: Int): Int {
-        return this + b
     }
 }
